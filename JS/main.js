6f2ab207f72c725f6c68f27b60c63bfc8e63c46d
Eliud Wanja <elkwanja@gmail.com>
2021-05-21 17:29:33 +0300

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,76 @@
+
+//Nav global variable
+const navigation = document.getElementById("navbar__list");
+
+//section global variable
+const sections = document.querySelectorAll("section");
+
+//build the nav
+const navBuilder = () => {
+  let navUI = "";
+
+  //loop over sections
+  sections.forEach((section) => {
+    const sectionID = section.id;
+    const sectionDataNav = section.dataset.nav;
+
+    navUI =
+      navUI +
+      `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+  });
+
+  //append all elements to navigation
+  navigation.innerHTML = navUI;
+};
+//call the function
+navBuilder();
+
+// Add class 'active' to section when near top of viewport
+
+//Return the size and elemet's position of an element
+const offset = (section) => {
+  return Math.floor(section.getBoundingClientRect().top);
+};
+
+//Remove the active class
+const removeActive = (section) => {
+  section.classList.remove("active");
+  section.style.cssText = "color: black;";
+};
+
+//Add active class
+const addActive = (condition, section) => {
+  if (condition) {
+    section.classList.add("active");
+    section.style.cssText = "color: #ff6600;";
+  }
+};
+
+//Implementing the actual function
+const sectionActivation = () => {
+  sections.forEach((section) => {
+    const elementOffset = offset(section);
+
+    inviewport = () => elementOffset < 200 && elementOffset >= -200;
+
+    removeActive(section);
+    addActive(inviewport(), section);
+  });
+};
+
+window.addEventListener("scroll", sectionActivation);
+
+// Scroll to anchor ID using scrollTO event
+const scrolling = () => {
+  const links = document.querySelectorAll(".navbar__menu a");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      for (i = 0; i < sections; i++) {
+        section[i].addEventListener("click", sectionScroll(link));
+      }
+    });
+  });
+};
+
+//call the function
+scrolling();
---
+++ readMe.md
@@ -0,0 +1,18 @@
+
+
+
+# The General Text
+This is the landing page project readme. It
+
+## Programming languages used:
+- HTML 
+- CSS 
+- JS
+
+## Functionalities:
+- Navigation
+- Dynamic active class
+- Scrolling effect
+
+## New skills learned throught this project:
+Well, we turned the static landing page into interactive landing page. 
